6ae4cff56a106204fca818f5502b0e825532d443
Lenodez <66466210+Lenodez@users.noreply.github.com>
2021-04-22 08:12:36 +0300

    First

---
+++ Assets/Scripts/MoveToPoint.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToPoint : MonoBehaviour
+{
+
+    public int speed = 1;
+    public GameObject player;//здесь ми указываем персонажа как игровой Object;
+
+    void Start()
+    {
+        player = (GameObject)this.gameObject; //тут присваиваем персонажа к игровому Object или как-то так.
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.position -= player.transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.position += player.transform.right * speed * Time.deltaTime;//персонаж плавно двигается на W,S,D,A;
+        }                                              //всё легко и просто, как борщ(всё как Вы и просили)
+    }
+}
